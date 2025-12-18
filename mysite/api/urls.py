60e125296fb4842961c48f52778d2a23431eb58b
Jaikumar Raja <kodupotuvaalbavan@gmail.com>
2025-12-19 05:29:41 +0530

    Add

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,8 +1,11 @@
+from os import name
 
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-path('blogposts/',views.BlogPostListCreate.as_view(),name='blogpost-view-create')
+path('blogposts/',views.BlogPostListCreate.as_view(),name='blogpost-view-create'),
+path('blogposts/<int:pk>/',views.BlogRetrieveUpdateDestroy.as_view(), name="update"),
+path('blog/',views.BlogPostList.as_view(), name="blog"),
 ]
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status, request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import BlogPost
 from .serializers import BlogPostSerializers
 # Create your views here.
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializers
+    def delete(self,request,*args,**kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class BlogRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializers
+    lookup_field = "pk"
+
+class BlogPostList(APIView):
+    def get(self,request):
+        title = request.query_params.get("title")
+        if title:
+            blog_posts = BlogPost.objects.all().filter(title__icontains=title)
+        else:
+            blog_posts = BlogPost.objects.all()
+        serializer = BlogPostSerializers(title,many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
